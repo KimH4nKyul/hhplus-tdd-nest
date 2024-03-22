2a7bda9dd75c5a7ff0c0c6e9ff452ca006687b80
KimH4nKyul <alskim93@kakao.com>
2024-03-22 20:44:57 +0900

    :recycle: point handler - mutex timout ms, and specific type

--- src/point/service/point.handler.ts
+++ src/point/service/point.handler.ts
@@ -6,11 +6,11 @@ export class PointHandler {
 
   private readonly mutexes: Map<UserId, MutexInterface> = new Map();
 
-  async charge(id: number, amount: number): Promise<UserPoint> {
+  async charge(id: UserId, amount: number): Promise<UserPoint> {
     this.isValid(id, amount);
 
     const mutex = this.mutexOf(id);
-    mutex.acquire();
+    await mutex.acquire();
     try {
       return await this.pointRepository.charge(id, amount);
     } finally {
@@ -18,10 +18,10 @@ export class PointHandler {
     }
   }
 
-  async use(id: number, amount: number): Promise<UserPoint> {
+  async use(id: UserId, amount: number): Promise<UserPoint> {
     this.isValid(id, amount);
     const mutex = this.mutexOf(id);
-    mutex.acquire();
+    await mutex.acquire();
     try {
       return await this.pointRepository.use(id, amount);
     } finally {
@@ -34,7 +34,7 @@ export class PointHandler {
     if (!mutex) {
       mutex = withTimeout(
         new Mutex(),
-        1000,
+        300,
         new Error(`시간 내에 뮤텍스를 얻지 못했습니다.`),
       );
       this.mutexes.set(id, mutex);
@@ -42,7 +42,7 @@ export class PointHandler {
     return mutex;
   }
 
-  private isValid(id: number, amount: number) {
+  private isValid(id: UserId, amount: number) {
     if (id < 0) throw new Error(`올바르지 않은 ID 값 입니다.`);
     if (amount < 0) throw new Error(`포인트가 0보다 작습니다.`);
   }
